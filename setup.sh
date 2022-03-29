f002325034e345291ea0d98904f8a1334f70ff6f
aldi Pratama <83162144+aldiipratama@users.noreply.github.com>
2022-03-29 11:36:56 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -24,13 +24,12 @@ apt update -y && apt upgrade -y
 pkg update -y && pkg upgrade -y
 pkg install python python2 python3 -y
 pip install requests
-mkdir spam-scw
-cd spam-scw
-
 echo -e "\e[1;34m[*] \e[32mmengcloning inti tools..\e[0m";
-sleep 2
-wget https://raw.githubusercontent.com/aldiipratama/spam-scw/main/run.py
+sleep 3
+git clone https://github.com/aldiipratama/spam-scw/
+cd spam-scw
 
 echo -e "\e[1;34m[√] \e[96msekarang ketik "python run.py !\e[0m";
 sleep 1
 echo -e "\e[1;34m[*] \e[32mselamat berspam ria! :D\e[0m";
+sleep 2
