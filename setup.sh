3e756fd57aa392257e5f59fc3693613775f14ba6
aldi Pratama <83162144+aldiipratama@users.noreply.github.com>
2022-03-29 12:48:35 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -16,20 +16,13 @@ echo -e "${RED}     ░     ░  ░           ░           ${ENDCOLOR}   "
                                           
 echo  ""                                          
 echo -e "\e[1;34m[*] \e[32msedang menginstall packages....\e[0m";
-sleep 1
-echo -e "\e[1;34m[*] \e[32mharap bersabar.. ini ujian..\e[0m";
 sleep 2
+echo -e "\e[1;34m[*] \e[32mharap bersabar.. ini ujian..\e[0m";
+sleep 3
 
 apt update -y && apt upgrade -y
 pkg update -y && pkg upgrade -y
 pkg install python python2 python3 -y
 pip install requests
-echo -e "\e[1;34m[*] \e[32mmengcloning inti tools..\e[0m";
-sleep 3
-git clone https://github.com/aldiipratama/spam-scw/
-cd spam-scw
 
-echo -e "\e[1;34m[√] \e[96msekarang ketik "python run.py !\e[0m";
-sleep 1
-echo -e "\e[1;34m[*] \e[32mselamat berspam ria! :D\e[0m";
-sleep 2
+echo -e "\e[1;34m[*] \e[36mDone!\e[0m";
